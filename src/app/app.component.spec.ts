d464c2a1ff117f74794c8ef64b0a59c11e568caa
joshoxe <joshoxenham1@gmail.com>
2020-11-08 16:44:31 +0000

    Set up testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cmpct-irc-client'`, () => {
+  it(`should have as title 'cmpct'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cmpct-irc-client');
+    expect(app.title).toEqual('cmpct');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('cmpct-irc-client app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('cmpct');
   });
 });
